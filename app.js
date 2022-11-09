06c486f4e0f47a42e0ad6231c5d880653283d765
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-11-10 00:54:14 +0600

    Modify

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function click(){
-    let save = document.getElementById('btn');
-    console.log(save.value);
-}
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function handlerClick(event){
+//     event.preventDefault();
+
+//     const firstNameInput = document.getElementById('first_name');
+//     console.log(firstNameInput.value);
+//     firstNameInput.value = '';
+
+// })
 
