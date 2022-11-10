f9c89c30c48c87c9ab5dde24867fda467401e37e
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-11-10 23:25:41 +0600

    Adding

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// const btn = document.getElementById('btn');
+const btn = document.getElementById('btn');
 
-// btn.addEventListener('click', function handlerClick(event){
-//     event.preventDefault();
+btn.addEventListener('click', function handlerClick(event){
+    event.preventDefault();
 
-//     const firstNameInput = document.getElementById('first_name');
-//     console.log(firstNameInput.value);
-//     firstNameInput.value = '';
-
-// })
-
-function save(){
-    let firstNameInput = document.getElementById('first_name');
+    const firstNameInput = document.getElementById('first_name');
     console.log(firstNameInput.value);
-    firstNameInput.value='';
-}
+    firstNameInput.value = '';
+
+})
+
+// function save(){
+//     let firstNameInput = document.getElementById('first_name');
+//     console.log(firstNameInput.value);
+//     firstNameInput.value='';
+// }
